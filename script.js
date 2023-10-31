79ade9fc6bd6ac20f3a3671b17cb96a237f6f05b
Nadezhda Ilina <nadiailina9@gmail.com>
2023-10-31 17:36:31 +0300

    fancybox delete

--- script.js
+++ script.js
@@ -1,4 +1,39 @@
 'use strict';
-$(document).ready(function () {
-  $('a#zoom').fancybox();
-}); //увеличение фото при нажатии
+//Встроенный слайдер Swiper
+var swiper = new Swiper('.mySwiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoHeight: true,
+  zoom: {
+    maxRatio: 5,
+  },
+});
+
+//Блок Программа курса, всплывающий текст через +
+function windowPlus() {
+  const headings = document.querySelectorAll('h3');
+
+  Array.prototype.forEach.call(headings, h => {
+    let btn = h.querySelector('button');
+    let target = h.nextElementSibling;
+
+    btn.onclick = () => {
+      let expanded = btn.getAttribute('aria-expanded') === 'true';
+
+      if (expanded) {
+        btn.textContent = '+'; // Изменяем текст кнопки на "+" при разворачивании
+        btn.style.fontSize = '4vw';
+      } else {
+        btn.textContent = '✖'; // Возвращаем текст кнопки на "✖" при сворачивании
+        btn.style.fontSize = '3vw';
+      }
+
+      btn.setAttribute('aria-expanded', !expanded);
+      target.hidden = expanded;
+    };
+  });
+}
+
+windowPlus();

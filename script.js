8991c6b3103bd31a5db4e312f2fdadf52fd5b4ef
Nadezhda Ilina <nadiailina9@gmail.com>
2023-11-25 19:15:43 +0300

    новые правки

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
 ("use strict");
+//Слайдер
+// Получаем все элементы слайдера
+const sliderContainers = document.querySelectorAll(".slider-container");
 
-//Встроенный слайдер Swiper
-var swiper = new Swiper(".mySwiper", {
-  modules: [Navigation, Pagination],
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoHeight: true,
-  zoom: {
-    maxRatio: 5,
-  },
+// Перебираем все контейнеры слайдера
+sliderContainers.forEach((sliderContainer) => {
+  // Получаем элементы слайдера для текущего контейнера
+  const slider = sliderContainer.querySelector(".slider");
+  const prevButton = sliderContainer.querySelector(".prev-button");
+  const nextButton = sliderContainer.querySelector(".next-button");
+  const slides = Array.from(slider.children);
+  const slideCount = slides.length;
+  let slideIndex = 0;
+
+  // Устанавливаем обработчики событий для кнопок текущего слайдера
+  prevButton.addEventListener("click", showPreviousSlide);
+  nextButton.addEventListener("click", showNextSlide);
+
+  // Функции для показа предыдущего и следующего слайда для текущего слайдера
+  function showPreviousSlide() {
+    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+    updateSlider();
+  }
+
+  function showNextSlide() {
+    slideIndex = (slideIndex + 1) % slideCount;
+    updateSlider();
+  }
+
+  // Функция для обновления отображения слайдера для текущего слайдера
+  function updateSlider() {
+    slides.forEach((slide, index) => {
+      if (index === slideIndex) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+
+  // Инициализация слайдера для текущего слайдера
+  updateSlider();
 });
 
 //Блок Программа курса, всплывающий текст через +
